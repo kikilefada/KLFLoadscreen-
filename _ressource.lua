7be2fe4cc36f9b9971f5823d4e32a090ac2d2252
Kiki Lefada <tonikkpro@gmail.com>
2020-03-25 22:49:31 +0100

    v1.0.0
    Ajout partie .Js 
    Modification incrémentation
    Rectification format Rétina
    Ajout [_ressource.lua]

---
+++ _ressource.lua
@@ -0,0 +1,14 @@
+description 'Ecran de chargement'
+
+files {
+
+    'index.html',
+    'style.css',
+    'main.js',
+    
+    'img/LOGO.png',
+    'background.png',
+
+}
+
+loadscreen 'index.html'
